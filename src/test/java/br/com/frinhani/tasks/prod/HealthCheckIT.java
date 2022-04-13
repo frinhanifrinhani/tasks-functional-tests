e68be3a69238ea1be5941b997c64ae776455be46
Frinhani <thiago.frinhani@ths.inf.br>
2022-04-13 11:52:05 -0300

    Ajuste verify

--- src/test/java/br/com/frinhani/tasks/prod/HealthCheckIT.java
+++ src/test/java/br/com/frinhani/tasks/prod/HealthCheckIT.java
@@ -8,7 +8,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 
@@ -16,10 +16,11 @@ public class HealthCheckIT {
 
 	@Test
 	public void healthCheck() throws MalformedURLException {
-		DesiredCapabilities cap = DesiredCapabilities.chrome();
-		WebDriver drive = new RemoteWebDriver(new URL("http://10.0.0.113:4444/wd/hub"), cap);
+		//DesiredCapabilities cap = DesiredCapabilities.chrome();
+		//WebDriver drive = new RemoteWebDriver(new URL("http://10.0.0.113:4444/wd/hub"), cap);
+		WebDriver drive = new ChromeDriver();
 		try {
-			drive.navigate().to("http://10.0.0.113:9999/tasks/");
+			drive.navigate().to("http://10.0.0.109:9999/tasks/");
 			drive.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			String version = drive.findElement(By.id("version")).getText();
